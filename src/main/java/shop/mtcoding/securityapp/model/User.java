613e23d9dd6862b811496f0253c46bff9f16d516
aozp73 <aozp73@naver.com>
2023-04-10 10:50:23 +0900

    User Entity 생성

---
+++ src/main/java/shop/mtcoding/securityapp/model/User.java
@@ -0,0 +1,54 @@
+package shop.mtcoding.securityapp.model;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import javax.persistence.Table;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@Entity
+@Table(name = "user_tb")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+    private String email;
+    private Boolean status;
+    private LocalDateTime createdAt; // LocalDateTime -> Timestamp 타입으로 자동으로 변환시켜준다.
+    private LocalDateTime updatedAt;
+
+    @PrePersist // insert 시에 동작
+    public void onCreate() {
+        this.createdAt = LocalDateTime.now();
+    }
+
+    @PreUpdate // update 시에 동작
+    public void onUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    @Builder
+    public User(Long id, String username, String password, String email, Boolean status, LocalDateTime createdAt,
+            LocalDateTime updatedAt) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.status = status;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+}
